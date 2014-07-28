19b582dda700a6dcca8b58c19d6adbc733302f27
hyrony <whsm1010@gmail.com>
2014-07-28 21:48:58 +0900

    Logo Changed, 주석 추가

--- SensingUI/src/com/example/sensingui/HomeActivity.java
+++ SensingUI/src/com/example/sensingui/HomeActivity.java
@@ -1,6 +1,5 @@
 package com.example.sensingui;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -20,31 +19,32 @@ public class HomeActivity extends FragmentActivity {
      * intensive, it may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
-    SectionsPagerAdapter mSectionsPagerAdapter;
+    SectionsPagerAdapter mSectionsPagerAdapter; //HomeActivity는 각 페이지별로 Section이 만들어진다.
 
     /**
      * The {@link ViewPager} that will host the section contents.
      */
-    ViewPager mViewPager;
+    ViewPager mViewPager; //각 페이지별로 상단에 페이지이름이 표시가 된다.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home); //기본 현태는 activity_home.xml에서 따온다.
 
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the app.
+        // 각 Section별로 Fragment들을 생성해서 만든다.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager = (ViewPager) findViewById(R.id.pager);//activity_home.xml에서 View의 id가 pager
+        mViewPager.setAdapter(mSectionsPagerAdapter); //ViewPager에 SectionPagerAdapter함수의 결과값을 넣음으로써 화면에 각 View를 뿌려준다.
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) { //Option버튼을 생성
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
@@ -55,7 +55,7 @@ public class HomeActivity extends FragmentActivity {
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
+    public class SectionsPagerAdapter extends FragmentStatePagerAdapter { //SectionPagerAdapter의 구성은 아래와 같다.
 
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
@@ -65,36 +65,36 @@ public class HomeActivity extends FragmentActivity {
         public Fragment getItem(int position) {
             switch (position) {
                 default:
-                case 0:
+                case 0: // 1번째 화면
                     return new SensingConditionView();
-                case 1:
+                case 1: // 2번째 화면
                     return new SensingOperationView();
-                case 2:
+                case 2: // 3번째 화면
                     return new ParallaxBackgroundFragment();
-                case 3:
+                case 3: // 4번째 화면
                     return new ParallaxBackgroundListFragment();
-                case 4:
+                case 4: // 5번째 화면
                     return new OverParallaxBackgroundListFragment();
             }
         }
 
         @Override
-        public int getCount() {
+        public int getCount() { //화면의 숫자는 5개
             return 5;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
-                case 0:
-                    return getString(R.string.title_section1);
-                case 1:
+                case 0: //1번째 화면의 제목
+                    return getString(R.string.title_section1); //1번째 화면의 제목
+                case 1: //2번째 화면의 제목
                     return getString(R.string.title_section2);
-                case 2:
+                case 2: //3번째 화면의 제목 
                     return getString(R.string.title_section3);
-                case 3:
+                case 3: //4번째 화면의 제목
                     return getString(R.string.title_section4);
-                case 4:
+                case 4: //5번째 화면의 제목
                     return getString(R.string.title_section5);
             }
             return null;
--- SensingUI/src/com/example/sensingui/LoginActivity.java
+++ SensingUI/src/com/example/sensingui/LoginActivity.java
@@ -11,47 +11,47 @@ import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 	
-	Button Resister,Login;
+	Button Resister,Login; // 버튼에 대한 상수 정의
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.login_main);
-	        
-	    Resister=(Button)findViewById(R.id.resbt);
-	    Resister.setOnClickListener(new View.OnClickListener() {
+	    setContentView(R.layout.login_main); // login-main이라는 이름을 가진 xml파일의 Layout을 화면에 출력
+	     //ID등록 버튼에 대한 기능 정의 
+	    Resister=(Button)findViewById(R.id.resbt); //xml의 버튼에 기능을 넣기 위해 Java의 Resister라는 변수에 xml의 resbt라는 id를 가진 버튼을 연결
+	    Resister.setOnClickListener(new View.OnClickListener() { //버튼이 클릭되기를 기다리는 함수
 			
-			@Override
-			public void onClick(View v) {
-				AlertDialog.Builder alert=new AlertDialog.Builder(LoginActivity.this);
-				alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+			@Override // 재정의
+			public void onClick(View v) { //클릭퇴었을 때
+				AlertDialog.Builder alert=new AlertDialog.Builder(LoginActivity.this); //alert라는 변수를 AlertDialog.Builder로 정의해 LoginActivity Class상에서 Dialog를 띄우는 형태로 생성
+				alert.setPositiveButton("확인", new DialogInterface.OnClickListener() { //긍정버튼란에 '확인'을 생성, 확인을 클릭할 경우를 기다림
 					@Override
-					public void onClick(DialogInterface dialog, int which) {
+					public void onClick(DialogInterface dialog, int which) { //확인이 클릭될 경우
 						// TODO Auto-generated method stub
-						dialog.dismiss();
+						dialog.dismiss(); // Alert를 무시, 아무 역할을 하지 않고 Alert을 닫음
 					}
 				});
-				alert.setMessage("E-mail과 Password를 등록하려면 Sixthsense팀으로 가입신청을 하시기 바랍니다.");
-				alert.show();
+				alert.setMessage("E-mail과 Password를 등록하려면 Sixthsense팀으로 가입신청을 하시기 바랍니다."); //Alert에 들어갈 내용을 작성
+				alert.show(); //Alert창을 보여줌
 			}
 		});
-	    
+	     //로그인 버튼에 대한 기능 정의
 	    Login=(Button)findViewById(R.id.loginbt);
 	    Login.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				EditText uemail = (EditText)findViewById(R.id.useremail);
-				EditText upassword = (EditText)findViewById(R.id.userpassword);
-				String semail=uemail.getText().toString();
-				String spassword=upassword.getText().toString();
-				if(semail.equals("sixthsense")&&spassword.equals("12345")){
-				Intent intent=new Intent(LoginActivity.this, HomeActivity.class);
-				startActivity(intent);
-				finish();
+				EditText uemail = (EditText)findViewById(R.id.useremail); //useremail이라는 id를 가진 xml의 edittext를 uemail이라는 변수에 부여
+				EditText upassword = (EditText)findViewById(R.id.userpassword); //upassword에 부여
+				String semail=uemail.getText().toString(); //uemail을 통해 받아온 text의 형식은 edittext이기 때문에 이를 String으로 변환해야만 함. 변환한 String값을 semail에 저장
+				String spassword=upassword.getText().toString(); //마찬가지로 spassword에 저장
+				if(semail.equals("sixthsense")&&spassword.equals("12345")){ //semail의 값이 a와 같고,spassword의 값이 b와 같다면 다음을 수행 
+				Intent intent=new Intent(LoginActivity.this, HomeActivity.class); //Login Activity에서 HomeActivityfh 전환하기 위한 Intent생성
+				startActivity(intent);//Intent를 수행함으로써 HomeActivity를 실행
+				finish(); //현재 LoginActivity를 닫음으로써 HomeActivity에서 뒤로버튼을 눌렀을 때 LoginActivity로 돌아오지 않고 바로 종료되도록 설정 
 				}
-				else{
+				else{ //ID나 Password가 주어진값과 다를 경우 Alert를 띄우는 역할을 한다.
 					AlertDialog.Builder alert=new AlertDialog.Builder(LoginActivity.this);
 					alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
 						@Override
